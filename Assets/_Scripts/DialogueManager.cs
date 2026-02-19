717704febb21e494723efbf55ce5d1fa5ddba594
chris-makes-games <chris.m.cox@hotmail.com>
2026-02-19 16:37:02 -0500

    Revert "Merge branch 'main' into Interaction"
    This reverts commit c8b142de070be5ef7948a3865ffcf7211adb081a, reversing
    changes made to 6a23d1fc7307f99af45fd59a7b7f7ad7e6cbea0b.

--- Assets/_Scripts/DialogueManager.cs
+++ Assets/_Scripts/DialogueManager.cs
@@ -1,56 +1,51 @@
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
-using System.Runtime.CompilerServices;
 
 public class DialogueManager : MonoBehaviour
 {
-     
+    //gmae objects to hold the panel and text and choices
     [SerializeField] private GameObject dialoguePanel;
-    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private GameObject choicePanel;
+    [SerializeField] private TextMeshProUGUI body;
+
+    //ink story JSON - recieves from the player
     private Story currentStory;
+
+    //keep track of if a story is playing - to wait when a player needs to make a choice
     private bool storyPlaying;
 
+    //singleton instance
     private static DialogueManager instance;
 
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = new DialogueManager();
-        }
-        instance = this;
+        instance = this;//ensure singleton
     }
 
     public static DialogueManager GetInstance()
     {
-        return instance;
+        return instance;//ensure singleton
     }
 
     private void Start()
     {
+        //starts inactive until text is shown
         storyPlaying = false;
-        dialoguePanel.SetActive(false);
+        choicePanel.SetActive(false);
+        body.text = "";
     }
 
-    public void EnterStoryMode(TextAsset ink)
-    {
+    
+    public void EnterStoryMode(TextAsset ink) //begin a story, accepts an ink
+    {    
         currentStory = new Story(ink.text);
         storyPlaying = true;
         dialoguePanel.SetActive(true);
-
-        if (currentStory.canContinue) 
-        {
-            dialogueText.text = currentStory.Continue();
-        }
-        else
-        {
-            ExitDialogueMode();
-        }
-
+        ContinueStory();
     }
 
-    private void Update()
+    private void Update() //brought in from the tutorial, not sure if needed
     {
         if (!storyPlaying)
         {
@@ -58,10 +53,26 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
-    private void ExitDialogueMode()
+    public void ExitStoryMode()//when a story is done, exits
     {
         storyPlaying = false;
-        dialoguePanel.SetActive(false);
-        dialogueText.text = "";
+        body.text = "";
+    }
+
+    private void ContinueStory() //goes to next step of story
+    {
+        if (currentStory.canContinue)
+        {
+            body.text = currentStory.Continue();
+        }
+        else
+        {
+            ExitStoryMode();
+        }
+    }
+
+    public void SetText(string s)
+    {
+        body.text = s;
     }
 }
