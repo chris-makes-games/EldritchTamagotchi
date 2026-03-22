4839ea42403f09aa528a04f53371c5dba73c3475
OTK5000 <owen.kraus@icloud.com>
2026-03-22 12:46:30 -0400

    simple start menu implementation
    Added a start menu with a functional start button, escape kills the game only on the start menu, can't be controlled with keyboard/controller, must use mouse

---
+++ Assets/_Scripts/StartMenuManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class StartMenuManager : MonoBehaviour
+{
+   [SerializeField] private InputActionAsset InputActions;
+   [SerializeField] private InputAction closeGame;
+
+    void Awake()
+    {
+        closeGame = InputSystem.actions.FindAction("Pause/Quit");
+    }
+
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // if you push the start button (on screen) it starts the game
+    public void StartGame()
+    {
+        Cursor.visible = false;
+        SceneManager.LoadScene("InitScene");
+        Debug.Log("Launching game...");
+    }
+
+    void Update()
+    {
+        // closes game on start menu if quit button is pressed (escape or start)
+        if (closeGame.WasPressedThisFrame())
+        {
+            Debug.Log("Quitting game...");
+            Application.Quit();
+        }
+    }
+}
