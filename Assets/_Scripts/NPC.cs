75aa991b71f3d5e9cf9657d04f092742a8056876
OTK5000 <owen.kraus@icloud.com>
2026-04-25 11:05:05 -0400

    bug fixes and more NPC work
    Finally fixed the dodge roll interaction bug. Animations don't bug out while interacting. You cannot move around while interacting anymore. Swapped out the NPC prefab for a new one that work a bit better (highlight still kinda fucked up but slightly better)

--- Assets/_Scripts/NPC.cs
+++ Assets/_Scripts/NPC.cs
@@ -10,16 +10,15 @@ public class NPC : MonoBehaviour
 
     public SpriteRenderer icon;
     public SpriteRenderer portrait;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
-        icon = GetComponent<SpriteRenderer>();
-        portrait = GetComponent<SpriteRenderer>();
+        // icon = GetComponent<SpriteRenderer>();
+        // portrait = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //maybe we can have animations play here, or have them wander
+        
     }
 }
