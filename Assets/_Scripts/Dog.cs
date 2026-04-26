fd3cc52cef797ace582be14f798df2d3f2bf5c29
OTK5000 <owen.kraus@icloud.com>
2026-04-26 13:19:43 -0400

    lots of stuff in evilscene
    You can kill the dog now! Utilized Ink logic related to killing the dog, stabbing the dog leaves behind a pool of blood and sadness. Not stabbing the dog leaves behind... a dog!

--- Assets/_Scripts/Dog.cs
+++ Assets/_Scripts/Dog.cs
@@ -1,20 +1,41 @@
 using UnityEngine;
 
 public class Dog : MonoBehaviour
-{
+{   
+    public static Dog instance;
     [SerializeField] private SpriteRenderer sr;
-    [SerializeField] private Sprite dog0, dog1;
+    [SerializeField] private Sprite dog0, dog1, dog2;
     private int animationFrameCounter = 0;
+
+    // death related variables
+    public bool dead = false;
+    private bool newDead = true;
+    [SerializeField] private Collider2D dogWall;
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     void FixedUpdate()
     {
-        animationFrameCounter++;
-        if (animationFrameCounter == 18)
+        if (!dead)
         {
-            if (sr.sprite == dog0) sr.sprite = dog1;
-            else if (sr.sprite == dog1) sr.sprite = dog0;
-            animationFrameCounter = 0;
+            animationFrameCounter++;
+            if (animationFrameCounter == 18)
+            {
+                if (sr.sprite == dog0) sr.sprite = dog1;
+                else if (sr.sprite == dog1) sr.sprite = dog0;
+                animationFrameCounter = 0;
+            }
+            if (animationFrameCounter >= 19) animationFrameCounter = 0;
         }
-        if (animationFrameCounter >= 19) animationFrameCounter = 0;
+        else if (newDead && dead)
+        {
+            sr.sprite = dog2;
+            dogWall.enabled = false;
+            transform.GetChild(1).gameObject.SetActive(false);
+            newDead = false;
+        }
     }
 }
